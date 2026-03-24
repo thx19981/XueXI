5788a4b42a3285f760c4698c386856d431682da1
tanghaoxuan <tanghaoxuan@huilianyi.com>
2026-03-24 13:21:16 +0800

    fix：学习-1

---
+++ src/main/java/com/xuexi/xuexi/XuexiApplication.java
@@ -0,0 +1,11 @@
+package com.xuexi.xuexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuexiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XuexiApplication.class, args);
+    }
+}
